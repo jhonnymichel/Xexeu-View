5142f691bbd4211da417e9007ebeaa5e16a045cf
Jhonny Michel <jhonnymichel@evolux.net.br>
2017-11-29 13:28:49 -0300

    using both modules and UMD in build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify-es'; 
+import uglify from 'rollup-plugin-uglify-es';
+const pkg = require('./package.json');
 
 export default {
   input: './src/index.js',
+  moduleName: 'Xexeu',
+  targets: [
+		{ dest: pkg.main, format: 'umd' },
+		{ dest: 'demo/xexeu-view.min.js', format: 'umd' },
+		{ dest: pkg.module, format: 'es' }
+	],
   output: {
     file: 'dist/xexeu-view.min.js',
     format: 'iife',
